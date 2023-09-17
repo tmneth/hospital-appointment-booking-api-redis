b2eb981f4951506004eba145d5d1f5bbdf51575f
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-17 14:20:13 +0300

    Add reserve routes

--- routes.js
+++ routes.js
@@ -6,11 +6,16 @@ import {
   getDoctor,
   deleteDoctor,
   getDoctors,
+  reserveAppointment,
+  removeReservation,
 } from "./controllers.js";
 
 router.post("/doctor", addDoctor);
 router.get("/doctor/:id", getDoctor);
 router.delete("/doctor/:id", deleteDoctor);
 router.get("/doctors", getDoctors);
+
+router.post("/reserve", reserveAppointment);
+router.delete("/reserve", removeReservation);
 
 export default router;
