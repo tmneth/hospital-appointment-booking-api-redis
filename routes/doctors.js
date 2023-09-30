8f77ca8109dd4b5881a1f3641b76f69c327d98a5
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-30 11:23:36 +0300

    Add route to get all doctor's reservations

--- routes/doctors.js
+++ routes/doctors.js
@@ -6,11 +6,13 @@ import {
   getDoctor,
   deleteDoctor,
   getDoctors,
+  getDoctorReservations,
 } from "../controllers/doctors.js";
 
 router.get("/", getDoctors);
 router.post("/", addDoctor);
 router.get("/:id", getDoctor);
 router.delete("/:id", deleteDoctor);
+router.get("/:id/reservations", getDoctorReservations);
 
 export default router;
